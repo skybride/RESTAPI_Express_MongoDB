1102789dea76fa79c7d6bf1908ebf541a867d7fc
sky <skybride@hotmail.com>
2021-02-08 17:51:04 -0500

    add db connection

--- server.js
+++ server.js
@@ -1,9 +1,27 @@
+require('dotenv/config');
+
 const express = require('express');
 const server = express();
 const port = 5000;
+const mongoose = require('mongoose');
+
+//Connect to DB
+mongoose.connect(
+    process.env.DATABASE_KEY, 
+    { useNewUrlParser: true,
+    useUnifiedTopology: true },
+    () => console.log("Connected to Database")
+);
 
 server.listen(port, () => {
     console.log('Started Server');
 });
 
+// //Routes
+// server.get('/', (req, res) => {
+//     res.send('HOME');
+// });
 
+// server.get('/posts', (req, res) => {
+//     res.send('Posts');
+// });
